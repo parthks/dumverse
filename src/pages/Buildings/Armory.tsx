ef9779798fe36452a2ff9ac37ebab433c6f36d68
parth <parthks@umich.edu>
2024-10-14 23:29:47 +0530

    armory character animation

--- src/pages/Buildings/Armory.tsx
+++ src/pages/Buildings/Armory.tsx
@@ -48,7 +48,7 @@ export default function Armory() {
           {/* Group the shop table, sign, and shopkeeper */}
           <div className="absolute" style={{ ...calculatePositionAndSize(50, 100, 48), transform: "translate(-50%, -100%)" }}>
             <img src="https://arweave.net/kcuv5Ifubl1oQ66gyRGfFusQfX9cm61RNwf1_Upsnl4" alt="Shop Table" className="absolute bottom-0" style={{ width: "100%", height: "auto" }} />
-            <div style={{ width: "300px", height: "300px" }} className="absolute left-1/2 h-auto -translate-x-1/2 -translate-y-[242%]">
+            <div style={{ width: "200px", height: "200px" }} className="absolute left-1/2 h-auto -translate-x-1/2 -translate-y-[221%]">
               <RiveShopKeeper />
             </div>
             {/* <Rive
@@ -131,7 +131,7 @@ import { useRive, Layout, Fit, Alignment } from "@rive-app/react-canvas";
 export const RiveShopKeeper = () => {
   const { RiveComponent } = useRive({
     // Load a local riv `clean_the_car.riv` or upload your own!
-    src: "https://public.rive.app/hosted/156163/191181/jwmTWZadb0eu_4gcTDnVow.riv",
+    src: "/character.riv",
     // Be sure to specify the correct state machine (or animation) name
     // This is optional.Provides additional layout control.
     layout: new Layout({
